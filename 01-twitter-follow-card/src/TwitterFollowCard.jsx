66a070473d7c91ca1d738ad5c17d133bc77e00dc
crisilto <crisilto1997@gmail.com>
2024-07-23 19:50:44 +0200

    Button update

--- 01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ 01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -1,21 +1,33 @@
-export function TwitterFollowCard({ userName, name, isFollowing }) {
-    
+import { useState } from "react"
+
+export function TwitterFollowCard({ children, userName, initialIsFollowing }) {
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
+
+    const text = isFollowing ? "Siguiendo" : "Seguir"
+    const buttonClassName = isFollowing ? "tw-followCard-button is-following" : "tw-followCard-button "
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
     return (
         <article className="tw-followCard">
             <header className="tw-followCard-header">
-                <img 
-                className="tw-followCard-avatar"
-                alt="El avatar de DeviantArt" 
-                src={`https://unavatar.io/${userName}`} />
+                <img
+                    className="tw-followCard-avatar"
+                    alt={`El avatar de ${userName}`}
+                    src={`https://unavatar.io/${userName}`} />
                 <div className="tw-followCard-info">
-                    <strong>{name}</strong>
+                    <strong>{children}</strong>
                     <span className="tw-followCard-infoUserName">@{userName}</span>
                 </div>
             </header>
 
             <aside>
-                <button className="tw-followCard-button">
-                    Seguir
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className='tw-followCard-text'>{text}</span>
+                    <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
                 </button>
             </aside>
         </article>
