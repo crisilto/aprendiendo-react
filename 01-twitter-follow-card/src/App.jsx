ce8a710ba639dcd89c06d89693c5b989570d8c8f
crisilto <crisilto1997@gmail.com>
2024-07-23 19:44:56 +0200

    Creation of the array of objects users

--- 01-twitter-follow-card/src/App.jsx
+++ 01-twitter-follow-card/src/App.jsx
@@ -1,28 +1,42 @@
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
+
+const users = [
+    {
+        userName: 'cucurella3',
+        name: 'Marc Cucurella',
+        isFollowing: true
+    },
+    {
+        userName: 'hugoduro14',
+        name: 'Hugo Duro',
+        isFollowing: false
+    },
+    {
+        userName: 'daniolmo7',
+        name: 'Dani Olmo',
+        isFollowing: false
+    },
+    {
+        userName: '5sergiob',
+        name: 'Sergio Busquets',
+        isFollowing: true
+    }
+]
+
 export function App() {
     return (
         <section className="App">
-            <TwitterFollowCard
-                isFollowing
-                userName="quienesfrankie"
-                name="F r a n k i e"
-            />
-            <TwitterFollowCard
-                isFollowing={false}
-                userName="alvaromassive"
-                name="alvaroO"
-            />
-            <TwitterFollowCard
-                isFollowing
-                userName="cucurella3"
-                name="Marc Cucurella"
-            />
-            <TwitterFollowCard
-                isFollowing={false}
-                userName="crisilto"
-                name="Cristina"
-            />
+            {
+                users.map(({ userName, name, isFollowing }) => (
+                    <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing} >
+                        {name}
+                    </TwitterFollowCard>
+                ))
+            }
         </section>
     );
 }
